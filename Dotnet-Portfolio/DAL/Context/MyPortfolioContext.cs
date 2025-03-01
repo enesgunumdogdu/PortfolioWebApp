81d723fe4c8eece7807ab2beb214bd7edbe4c506
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-02 00:00:03 +0300

    initial migration

---
+++ Dotnet-Portfolio/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,23 @@
+﻿using Dotnet_Portfolio.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_Portfolio.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;initial catalog=MyPortfolioDb;Integrated Security=True;");
+        }
+        public DbSet<About> Abouts{ get; set; }
+        public DbSet<Contact> Contacts{ get; set; }
+        public DbSet<Experience> Experinces{ get; set; }
+        public DbSet<Feature> Features{ get; set; }
+        public DbSet<Message> Messages{ get; set; }
+        public DbSet<Portfolio> Portfolios{ get; set; }
+        public DbSet<Skill> Skills{ get; set; }
+        public DbSet<SocialMedia> SocialMedias{ get; set; }
+        public DbSet<Testimonial> Testimonials{ get; set; }
+
+    }
+}
