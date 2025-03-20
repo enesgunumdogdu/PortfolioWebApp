fff79d62522351acc1d8bbe2c50d0ab058006387
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-20 17:11:51 +0300

    Created _TestimonialComponentPartial.cs for new VeiwComponent

---
+++ Dotnet-Portfolio/ViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Portfolio.ViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
