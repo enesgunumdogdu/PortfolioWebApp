0b3653af3ba62fd6fa3b74c82d2a66e655f7c982
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-20 17:21:52 +0300

    Created _FooterComponentPartial.cs for new ViewComponent

---
+++ Dotnet-Portfolio/ViewComponents/_FooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Portfolio.ViewComponents
+{
+    public class _FooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
