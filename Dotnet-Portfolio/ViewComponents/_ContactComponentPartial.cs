4857ee5fe1644e04b34c7a1b5e554b78ae002d28
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-20 17:19:09 +0300

    Created _ContactComponentPartial.cs for new ViewComponent

---
+++ Dotnet-Portfolio/ViewComponents/_ContactComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Portfolio.ViewComponents
+{
+    public class _ContactComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
