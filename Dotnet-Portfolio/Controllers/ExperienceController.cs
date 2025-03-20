b5cb0bfe1f522e06b9c78ce6deaaff2807b71fc2
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-21 00:02:30 +0300

    ExperienceController, LayoutController.cs added.

---
+++ Dotnet-Portfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Portfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult ExperienceList()
+        {
+            return View();
+        }
+    }
+}
