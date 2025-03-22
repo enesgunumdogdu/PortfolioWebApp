9a230bb18ef17b30b42e2f509b6626357fb6a79e
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-23 00:10:37 +0300

    Refactor script inclusion with new view component
    Removed direct script tags from `Index.cshtml` and replaced them with a new `_LayoutScriptComponentPartial` view component for better organization. Added the necessary script tags to `Default.cshtml` to maintain functionality while promoting modularity in the code structure.

---
+++ Dotnet-Portfolio/ViewComponents/LayoutViewComponents/_LayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Portfolio.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
