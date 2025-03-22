ea4faf279335a7581c9d80b9279a4d5d30e190b7
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-22 23:50:39 +0300

    Refactor HTML layout with view component for head section
    Replaced static HTML head in `Index.cshtml` with a new view component, `_LayoutHeadComponentPartial`, to enhance separation of concerns and reusability. Added a new head section in `Default.cshtml` with necessary meta tags and stylesheet links.

---
+++ Dotnet-Portfolio/ViewComponents/LayoutViewComponents/_LayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Portfolio.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
