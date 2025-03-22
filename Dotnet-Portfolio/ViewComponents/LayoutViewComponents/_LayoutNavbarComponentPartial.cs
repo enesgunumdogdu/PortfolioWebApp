044a94e1b384a825bf0991e1d20a4986895d358f
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-22 23:59:13 +0300

    Refactor navigation into a view component
    Modularized the main header and navigation into a new view component `_LayoutNavbarComponentPartial` for improved organization and reusability. Removed the original navigation code from `Index.cshtml` and updated it to invoke the new component. Created a new `Default.cshtml` file to maintain a consistent layout across the application.

---
+++ Dotnet-Portfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Portfolio.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
