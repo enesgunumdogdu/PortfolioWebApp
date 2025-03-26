6255fc91d684fa0104ce7a523abe815b6f06413c
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-03-26 23:53:12 +0300

    Add statistics to StatisticController
    - Introduced MyPortfolioContext to access data.
    - Added ViewBag properties for skills and message counts.
    - Enhanced Index action to provide read/unread message statistics.

--- Dotnet-Portfolio/Controllers/StatisticController.cs
+++ Dotnet-Portfolio/Controllers/StatisticController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dotnet_Portfolio.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dotnet_Portfolio.Controllers
 {
     public class StatisticController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
+
         public IActionResult Index()
         {
+            ViewBag.v1 = context.Skills.Count();
+            ViewBag.v2 = context.Messages.Count();
+            ViewBag.v3 = context.Messages.Where(x=> x.isRead == false).Count();
+            ViewBag.v4 = context.Messages.Where(x=> x.isRead == true).Count();
             return View();
         }
     }
