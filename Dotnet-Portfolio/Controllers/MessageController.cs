9cd0a036cabffa214156118aa3bb7a4436d2b5b5
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-04-07 21:02:46 +0300

    Add MessageController and Inbox view for message display
    Introduces a new `MessageController` with an `Inbox` action
    to retrieve and display messages from the database. A new
    Razor view `Inbox.cshtml` is created to present messages
    in a table format, including action buttons for viewing,
    updating, and deleting messages.

---
+++ Dotnet-Portfolio/Controllers/MessageController.cs
@@ -0,0 +1,16 @@
+﻿using Dotnet_Portfolio.DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+
+        public IActionResult Inbox()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+    }
+}
